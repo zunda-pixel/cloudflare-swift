a4079f195a0770284e1bae30aeabd85512f150e9
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-04-25 21:08:28 +0900

    add image endpoint

---
+++ Sources/Images/Images/Image.swift
@@ -0,0 +1,28 @@
+import Foundation
+import HTTPTypes
+import HTTPTypesFoundation
+
+extension ImageClient {
+  /// Image
+  /// https://developers.cloudflare.com/api/operations/cloudflare-images-image-details
+  /// - Parameter imageId: Image ID
+  /// - Returns: ``Image``
+  public func image(id imageId: String) async throws -> Image {
+    let url = URL(string: "https://api.cloudflare.com/client/v4/accounts/\(accountId)/images/v1/\(imageId)")!
+    
+    let request = HTTPRequest(
+      method: .get,
+      url: url,
+      headerFields: HTTPFields(dictionaryLiteral: (.authorization, "Bearer \(token)"))
+    )
+    
+    let (data, _) = try await URLSession.shared.data(for: request)
+    
+    let response = try JSONDecoder.images.decode(ImagesResponse<Image>.self, from: data)
+    if let result = response.result, response.success {
+      return result
+    } else {
+      throw handleError(errors: response.errors)
+    }
+  }
+}
