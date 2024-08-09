35e70ee81ae2e9f8282647acef4f141a7bef7786
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-10 04:38:06 +0900

    replace to swift-testing

--- Tests/ImagesClientTests/ImagesClientTests.swift
+++ Tests/ImagesClientTests/ImagesClientTests.swift
@@ -1,183 +1,194 @@
 import HTTPTypes
 import HTTPTypesFoundation
-import XCTest
-
-@testable import ImagesClient
+import ImagesClient
+import Foundation
+import Testing
 
 #if canImport(FoundationNetworking)
   import FoundationNetworking
 #endif
 
-final class ImagesClientTests: XCTestCase {
-  let client = ImagesClient(
-    apiToken: ProcessInfo.processInfo.environment["IMAGES_API_TOKEN"]!,
-    accountId: ProcessInfo.processInfo.environment["ACCOUNT_ID"]!,
-    httpClient: .urlSession(.shared)
+let client = ImagesClient(
+  apiToken: ProcessInfo.processInfo.environment["IMAGES_API_TOKEN"]!,
+  accountId: ProcessInfo.processInfo.environment["ACCOUNT_ID"]!,
+  httpClient: .urlSession(.shared)
+)
+
+let cloudflareLogoURL: URL = URL(
+  string:
+    "https://cf-assets.www.cloudflare.com/slt3lc6tev37/7bIgGp4hk4SFO0o3SBbOKJ/b48185dcf20c579960afad879b25ea11/CF_logo_stacked_blktype.jpg"
+)!
+var coudflareLogoName: String { cloudflareLogoURL.lastPathComponent }
+
+var samplePng: Data {
+  let filePath = Bundle.module.url(forResource: "Swift_logo", withExtension: "svg")!
+  return try! Data(contentsOf: filePath)
+}
+
+@Test
+func uploadData() async throws {
+  let metadatas = ["test1": "test2"]
+  let response = try await client.upload(
+    imageData: samplePng,
+    metadatas: metadatas
+  )
+  #expect(response.metadatas == metadatas)
+  #expect(response.fileName == nil)
+  #expect(response.requireSignedURLs == false)
+  #expect(response.variants.isEmpty == false)
+}
+
+@Test
+func uploadURL() async throws {
+  let metadatas = ["test1": "test2"]
+  let response = try await client.upload(
+    imageURL: cloudflareLogoURL,
+    metadatas: metadatas
+  )
+  #expect(response.metadatas == metadatas)
+  #expect(response.fileName == coudflareLogoName)
+  #expect(response.requireSignedURLs == false)
+  #expect(response.variants.isEmpty ==  false)
+}
+
+@Test
+func uploadDataWithId() async throws {
+  let id = String(Int.random(in: Int.min..<Int.max))
+  let response = try await client.upload(imageData: samplePng, id: id)
+  #expect(id == response.id)
+  #expect(response.metadatas == [:])
+  #expect(response.fileName == nil)
+  #expect(response.requireSignedURLs == false)
+  #expect(response.variants.isEmpty == false)
+}
+
+@Test
+func uploadURLWithId() async throws {
+  let id = String(Int.random(in: Int.min..<Int.max))
+  let response = try await client.upload(imageURL: cloudflareLogoURL, id: id)
+  #expect(id == response.id)
+  #expect(response.metadatas == [:])
+  #expect(response.fileName == coudflareLogoName)
+  #expect(response.requireSignedURLs ==  false)
+  #expect(response.variants.isEmpty == false)
+}
+
+@Test
+func uploadDataWithRequireSignedURLs() async throws {
+  let response = try await client.upload(
+    imageData: samplePng,
+    requireSignedURLs: true
+  )
+  #expect(response.metadatas == [:])
+  #expect(response.fileName == nil)
+  #expect(response.requireSignedURLs == true)
+  #expect(response.variants.isEmpty == false)
+}
+
+@Test
+func uploadURLWithRequireSignedURLs() async throws {
+  let response = try await client.upload(
+    imageURL: cloudflareLogoURL,
+    requireSignedURLs: true
+  )
+  #expect(response.metadatas == [:])
+  #expect(response.fileName == coudflareLogoName)
+  #expect(response.requireSignedURLs == true)
+  #expect(response.variants.isEmpty == false)
+}
+
+@Test
+func deleteImage() async throws {
+  let response = try await client.upload(imageURL: cloudflareLogoURL)
+  try await client.delete(id: response.id)
+}
+
+func fetchImages() async throws {
+  let response1 = try await client.images(perPage: 10)
+  #expect(response1.images.count == 10)
+  let response2 = try await client.images(
+    continuationToken: response1.continuationToken,
+    perPage: 10
+  )
+  #expect(response2.images.count == 10)
+}
+
+@Test
+func fetchImage() async throws {
+  let uploadedImage = try await client.upload(imageData: samplePng)
+  let image = try await client.image(id: uploadedImage.id)
+  // TODO need to fix Cloudflare Images API.
+  // Upload Images's response uploaded date thas has one second more.
+  // #expect(uploadedImage == image)
+  #expect(image.id == uploadedImage.id)
+  #expect(image.fileName == uploadedImage.fileName)
+  #expect(image.metadatas == uploadedImage.metadatas)
+  // #expect(image.uploadedDate == uploadedImage.uploadedDate)
+  #expect(image.requireSignedURLs == uploadedImage.requireSignedURLs)
+  #expect(image.variants == uploadedImage.variants)
+}
+
+@Test
+func updateImage() async throws {
+  let metadatas = ["key1": "value1"]
+  let requireSignedURLs = true
+
+  let uploadedImage = try await client.upload(imageData: samplePng, requireSignedURLs: false)
+  let image = try await client.update(
+    id: uploadedImage.id,
+    metadatas: metadatas,
+    requireSignedURLs: requireSignedURLs
   )
 
-  let cloudflareLogoURL: URL = URL(
-    string:
-      "https://cf-assets.www.cloudflare.com/slt3lc6tev37/7bIgGp4hk4SFO0o3SBbOKJ/b48185dcf20c579960afad879b25ea11/CF_logo_stacked_blktype.jpg"
-  )!
-  var coudflareLogoName: String { cloudflareLogoURL.lastPathComponent }
+  #expect(image.metadatas == metadatas)
+  #expect(image.requireSignedURLs == requireSignedURLs)
+}
 
-  var samplePng: Data {
-    let filePath = Bundle.module.url(forResource: "Swift_logo", withExtension: "svg")!
-    return try! Data(contentsOf: filePath)
-  }
+@Test
+func usageStats() async throws {
+  let (allowedImageCount, currentImageCount) = try await client.usageStats()
+  #expect(allowedImageCount > 0)
+  #expect(currentImageCount > 0)
+}
 
-  func testUploadData() async throws {
-    let metadatas = ["test1": "test2"]
-    let response = try await client.upload(
-      imageData: samplePng,
-      metadatas: metadatas
-    )
-    XCTAssertEqual(response.metadatas, metadatas)
-    XCTAssertNil(response.fileName)
-    XCTAssertEqual(response.requireSignedURLs, false)
-    XCTAssertTrue(!response.variants.isEmpty)
-  }
+@Test
+func baseImage() async throws {
+  let uploadedImage = try await client.upload(imageData: samplePng, requireSignedURLs: false)
+  _ = try await client.baseImage(id: uploadedImage.id)
+}
 
-  func testUploadURL() async throws {
-    let metadatas = ["test1": "test2"]
-    let response = try await client.upload(
-      imageURL: cloudflareLogoURL,
-      metadatas: metadatas
-    )
-    XCTAssertEqual(response.metadatas, metadatas)
-    XCTAssertEqual(response.fileName, coudflareLogoName)
-    XCTAssertEqual(response.requireSignedURLs, false)
-    XCTAssertTrue(!response.variants.isEmpty)
-  }
+@Test
+func createAuthenticatedUploadURLWithURL() async throws {
+  let metadatas = ["key1": "value1"]
+  let requireSignedURLs = true
+  let (id, uploadURL) = try await client.createAuthenticatedUploadURL(
+    metadatas: metadatas,
+    requireSignedURLs: requireSignedURLs
+  )
+  let result = try await ImagesClient.upload(
+    uploadURL: uploadURL,
+    imageURL: cloudflareLogoURL,
+    httpClient: .urlSession(.shared)
+  )
+  #expect(result.id == id)
+  #expect(result.metadatas == metadatas)
+  #expect(result.requireSignedURLs == requireSignedURLs)
+}
 
-  func testUploadDataWithId() async throws {
-    let id = String(Int.random(in: Int.min..<Int.max))
-    let response = try await client.upload(imageData: samplePng, id: id)
-    XCTAssertEqual(id, response.id)
-    XCTAssertEqual(response.metadatas, [:])
-    XCTAssertNil(response.fileName)
-    XCTAssertEqual(response.requireSignedURLs, false)
-    XCTAssertTrue(!response.variants.isEmpty)
-  }
-
-  func testUploadURLWithId() async throws {
-    let id = String(Int.random(in: Int.min..<Int.max))
-    let response = try await client.upload(imageURL: cloudflareLogoURL, id: id)
-    XCTAssertEqual(id, response.id)
-    XCTAssertEqual(response.metadatas, [:])
-    XCTAssertEqual(response.fileName, coudflareLogoName)
-    XCTAssertEqual(response.requireSignedURLs, false)
-    XCTAssertTrue(!response.variants.isEmpty)
-  }
-
-  func testUploadDataWithRequireSignedURLs() async throws {
-    let response = try await client.upload(
-      imageData: samplePng,
-      requireSignedURLs: true
-    )
-    XCTAssertEqual(response.metadatas, [:])
-    XCTAssertNil(response.fileName)
-    XCTAssertEqual(response.requireSignedURLs, true)
-    XCTAssertTrue(!response.variants.isEmpty)
-  }
-
-  func testUploadURLWithRequireSignedURLs() async throws {
-    let response = try await client.upload(
-      imageURL: cloudflareLogoURL,
-      requireSignedURLs: true
-    )
-    XCTAssertEqual(response.metadatas, [:])
-    XCTAssertEqual(response.fileName, coudflareLogoName)
-    XCTAssertEqual(response.requireSignedURLs, true)
-    XCTAssertTrue(!response.variants.isEmpty)
-  }
-
-  func testDeleteImage() async throws {
-    let response = try await client.upload(imageURL: cloudflareLogoURL)
-    try await client.delete(id: response.id)
-  }
-
-  func testFetchImages() async throws {
-    let response1 = try await client.images(perPage: 10)
-    XCTAssertEqual(response1.images.count, 10)
-    let response2 = try await client.images(
-      continuationToken: response1.continuationToken,
-      perPage: 10
-    )
-    XCTAssertEqual(response2.images.count, 10)
-  }
-
-  func testFetchImage() async throws {
-    let uploadedImage = try await client.upload(imageData: samplePng)
-    let image = try await client.image(id: uploadedImage.id)
-    // TODO need to fix Cloudflare Images API.
-    // Upload Images's response uploaded date thas has one second more.
-    // XCTAssertEqual(uploadedImage, image)
-    XCTAssertEqual(image.id, uploadedImage.id)
-    XCTAssertEqual(image.fileName, uploadedImage.fileName)
-    XCTAssertEqual(image.metadatas, uploadedImage.metadatas)
-    // XCTAssertEqual(image.uploadedDate, uploadedImage.uploadedDate)
-    XCTAssertEqual(image.requireSignedURLs, uploadedImage.requireSignedURLs)
-    XCTAssertEqual(image.variants, uploadedImage.variants)
-  }
-
-  func testUpdateImage() async throws {
-    let metadatas = ["key1": "value1"]
-    let requireSignedURLs = true
-
-    let uploadedImage = try await client.upload(imageData: samplePng, requireSignedURLs: false)
-    let image = try await client.update(
-      id: uploadedImage.id,
-      metadatas: metadatas,
-      requireSignedURLs: requireSignedURLs
-    )
-
-    XCTAssertEqual(image.metadatas, metadatas)
-    XCTAssertEqual(image.requireSignedURLs, requireSignedURLs)
-  }
-
-  func testUsageStats() async throws {
-    let (allowedImageCount, currentImageCount) = try await client.usageStats()
-    XCTAssertTrue(allowedImageCount > 0)
-    XCTAssertTrue(currentImageCount > 0)
-  }
-
-  func testBaseImage() async throws {
-    let uploadedImage = try await client.upload(imageData: samplePng, requireSignedURLs: false)
-    _ = try await client.baseImage(id: uploadedImage.id)
-  }
-
-  func testCreateAuthenticatedUploadURLWithURL() async throws {
-    let metadatas = ["key1": "value1"]
-    let requireSignedURLs = true
-    let (id, uploadURL) = try await client.createAuthenticatedUploadURL(
-      metadatas: metadatas,
-      requireSignedURLs: requireSignedURLs
-    )
-    let result = try await ImagesClient.upload(
-      uploadURL: uploadURL,
-      imageURL: cloudflareLogoURL,
-      httpClient: .urlSession(.shared)
-    )
-    XCTAssertEqual(result.id, id)
-    XCTAssertEqual(result.metadatas, metadatas)
-    XCTAssertEqual(result.requireSignedURLs, requireSignedURLs)
-  }
-
-  func testCreateAuthenticatedUploadURLWithData() async throws {
-    let metadatas = ["key1": "value1"]
-    let requireSignedURLs = true
-    let (id, uploadURL) = try await client.createAuthenticatedUploadURL(
-      metadatas: metadatas,
-      requireSignedURLs: requireSignedURLs
-    )
-    let result = try await ImagesClient.upload(
-      uploadURL: uploadURL,
-      imageData: samplePng,
-      httpClient: .urlSession(.shared)
-    )
-    XCTAssertEqual(result.id, id)
-    XCTAssertEqual(result.metadatas, metadatas)
-    XCTAssertEqual(result.requireSignedURLs, requireSignedURLs)
-  }
+@Test
+func createAuthenticatedUploadURLWithData() async throws {
+  let metadatas = ["key1": "value1"]
+  let requireSignedURLs = true
+  let (id, uploadURL) = try await client.createAuthenticatedUploadURL(
+    metadatas: metadatas,
+    requireSignedURLs: requireSignedURLs
+  )
+  let result = try await ImagesClient.upload(
+    uploadURL: uploadURL,
+    imageData: samplePng,
+    httpClient: .urlSession(.shared)
+  )
+  #expect(result.id == id)
+  #expect(result.metadatas == metadatas)
+  #expect(result.requireSignedURLs == requireSignedURLs)
 }
