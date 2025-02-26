5f5ad873eb410102a2b20a6d2c893e8efc42fb51
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-02-26 22:28:21 +0900

    replace to Date.ISO8601FormatStyle from DateFormatter

--- Sources/ImagesClient/JSONDecoder++.swift
+++ Sources/ImagesClient/JSONDecoder++.swift
@@ -6,10 +6,8 @@ extension JSONDecoder {
     decoder.dateDecodingStrategy = .custom { decoder in
       let container = try decoder.singleValueContainer()
       let string = try container.decode(String.self)
-      let formatter = ISO8601DateFormatter()
-      formatter.formatOptions.insert(.withFractionalSeconds)
-
-      return formatter.date(from: string)!
+      let dateFormatStyle: Date.ISO8601FormatStyle = .iso8601.year().month().day().time(includingFractionalSeconds: true)
+      return try Date(string, strategy: dateFormatStyle)
     }
 
     return decoder
