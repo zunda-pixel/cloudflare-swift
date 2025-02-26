156e57093addfe5d5b5c5122d3e44a28eef3f2f6
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-02-26 22:32:48 +0900

    format

--- Sources/ImagesClient/JSONDecoder++.swift
+++ Sources/ImagesClient/JSONDecoder++.swift
@@ -6,8 +6,10 @@ extension JSONDecoder {
     decoder.dateDecodingStrategy = .custom { decoder in
       let container = try decoder.singleValueContainer()
       let string = try container.decode(String.self)
-      let dateFormatStyle: Date.ISO8601FormatStyle = .iso8601.year().month().day().time(includingFractionalSeconds: true)
-      return try Date(string, strategy: dateFormatStyle)
+      return try Date(
+        string,
+        strategy: .iso8601.year().month().day().time(includingFractionalSeconds: true)
+      )
     }
 
     return decoder
