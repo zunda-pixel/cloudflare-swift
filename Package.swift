e78a2ef0611ac33ad0a33099624da72a8dce1647
zunda <47569369+zunda-pixel@users.noreply.github.com>
2025-06-04 18:10:09 +0900

    update libarary (#33)

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-http-types", from: "1.3.0"),
-    .package(url: "https://github.com/vapor/multipart-kit", from: "5.0.0-alpha.3"),
+    .package(url: "https://github.com/vapor/multipart-kit", from: "5.0.0-alpha.5"),
     .package(url: "https://github.com/zunda-pixel/http-client", from: "0.3.0"),
   ],
   targets: [
