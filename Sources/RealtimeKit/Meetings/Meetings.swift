068c17ecf8e6e3c0ad889269219cba6273fc939b
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-06-27 16:32:33 +0900

    add getParticipants, getParticipant, addParticipants

--- Sources/RealtimeKit/Meetings/Meetings.swift
+++ Sources/RealtimeKit/Meetings/Meetings.swift
@@ -54,5 +54,65 @@ extension Client {
     let response = try JSONDecoder().decode(SingleResponse<Meeting>.self, from: data)
     return response.data
   }
+  
+  public func participants(
+    for meetingId: Meeting.ID,
+    pageNo: Int? = nil,
+    perPage: Int? = nil
+  )  async throws -> ListResponse<User> {
+    let url = baseURL
+      .appendingPathComponent("meetings/\(meetingId)/participants")
+      .appending(queryItems: [
+        .init(name: "page_no", value: pageNo.map { String($0) }),
+        .init(name: "per_page", value: perPage.map { String($0) })
+      ].compactMap({ $0.value != nil ? $0 : nil }))
+    let request = HTTPRequest(
+      method: .get,
+      url: url,
+      headerFields: [
+        .contentType: "application/json"
+      ]
+    )
+    let (data, _) = try await execute(request)
+    let response = try JSONDecoder().decode(ListResponse<User>.self, from: data)
+    return response
+  }
+  
+  public func addParticipant(
+    for meetingId: Meeting.ID,
+    participant: NewUser
+  )  async throws -> User {
+    let url = baseURL.appendingPathComponent("meetings/\(meetingId)/participants")
+
+    let request = HTTPRequest(
+      method: .post,
+      url: url,
+      headerFields: [
+        .contentType: "application/json"
+      ]
+    )
+    let bodyData = try JSONEncoder().encode(participant)
+    let (data, _) = try await execute(request, body: bodyData)
+    let response = try JSONDecoder().decode(SingleResponse<User>.self, from: data)
+    return response.data
+  }
+  
+  public func participant(
+    for meetingId: Meeting.ID,
+    participantId: User.ID
+  )  async throws -> User {
+    let url = baseURL.appendingPathComponent("meetings/\(meetingId)/participants/\(participantId)")
+
+    let request = HTTPRequest(
+      method: .get,
+      url: url,
+      headerFields: [
+        .contentType: "application/json"
+      ]
+    )
+    let (data, _) = try await execute(request)
+    let response = try JSONDecoder().decode(SingleResponse<User>.self, from: data)
+    return response.data
+  }
 }
 
