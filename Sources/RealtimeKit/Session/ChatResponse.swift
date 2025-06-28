821bffbb1652f7547c16fcca7f1bf2ee038feb71
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-06-28 16:45:30 +0900

    fix property name

--- Sources/RealtimeKit/Session/ChatResponse.swift
+++ Sources/RealtimeKit/Session/ChatResponse.swift
@@ -1,17 +1,17 @@
 import Foundation
 
 public struct ChatResponse: Decodable {
-  public var chatDownloadURL: URL
-  public var chatDownloadExpiry: Date
+  public var downloadURL: URL
+  public var downloadExpiry: Date
   
   enum CodingKeys: String, CodingKey {
-    case chatDownloadURL = "chat_download_url"
-    case chatDownloadExpiry = "chat_download_url_expiry"
+    case downloadURL = "chat_download_url"
+    case downloadExpiry = "chat_download_url_expiry"
   }
   
   public init(from decoder: any Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
-    self.chatDownloadURL = try container.decode(URL.self, forKey: .chatDownloadURL)
-    self.chatDownloadExpiry = try Date(container.decode(String.self, forKey: .chatDownloadExpiry), strategy: .iso8601WithFractionalSeconds)
+    self.downloadURL = try container.decode(URL.self, forKey: .downloadURL)
+    self.downloadExpiry = try Date(container.decode(String.self, forKey: .downloadExpiry), strategy: .iso8601WithFractionalSeconds)
   }
 }
